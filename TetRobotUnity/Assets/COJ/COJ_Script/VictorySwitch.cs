817add502fc11f0e55641dd69c5493c55bc39fcd
Corentin <job.corentin@outlook.fr>
2020-02-02 15:19:22 +0100

    à merge fast

---
+++ TetRobotUnity/Assets/COJ/COJ_Script/VictorySwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictorySwitch : MonoBehaviour
+{
+    private void OnDisable()
+    {
+
+        if (gameObject.CompareTag("Pilote1"))
+        {
+            SceneManager.LoadScene(3);
+        }
+        else if (gameObject.CompareTag("Pilote2"))
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
---
+++ TetRobotUnity/Assets/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(load());
+    }
+
+    IEnumerator load()
+    {
+        yield return new WaitForSeconds(7f);
+        SceneManager.LoadScene(0);
+
+    }
+    }
